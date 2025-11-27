523ab999042f7abfc1c2ef17fd9863c09d7feea8
vanessalobo <vanessa.lobo0211@gmail.com>
2025-11-27 10:57:10 -0600

    Add common ui steps

--- features/step_definitions/ui_steps.rb
+++ features/step_definitions/ui_steps.rb
@@ -1,6 +1,4 @@
-When(/I press "([^"]+)"/) do |button|
-  # Click a button by text or value anywhere on the page. Prefer `click_button` which
-  # matches <button> and <input type="submit"> elements; fall back to `click_on`.
+When(/^I press "([^"]+)"$/) do |button|
   begin
     click_button button
   rescue Capybara::ElementNotFound
@@ -9,15 +7,18 @@ When(/I press "([^"]+)"/) do |button|
 end
 
 
+Then(/I should see "([^"]+)"/) do |text|
+  expect(page).to have_content(text)
+end
+
+
 Then(/I should see a flash message matching (\/.+\/i?)/) do |regex_literal|
-  # Accepts a literal regex string like /Playlist created/i
   if regex_literal =~ %r{^/(.*)/([i]?)$}
     pattern = Regexp.new($1, $2 == 'i' ? Regexp::IGNORECASE : 0)
   else
     pattern = Regexp.new(regex_literal)
   end
 
-  # Flash messages render in .flash-div as .flash-notice or .flash-alert
   expect(page).to have_css('.flash-div')
   text = page.find('.flash-div').text
   expect(text).to match(pattern)
