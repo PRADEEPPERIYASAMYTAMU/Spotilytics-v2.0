95828648432963ba714dcbed9f70d8729b26d8d6
PRADEEPPERIYASAMYTAMU <papradeep11cs@tamu.edu>
2025-12-02 16:51:17 -0600

    Configure SimpleCov for test coverage reporting

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,20 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'simplecov'
+require 'simplecov'
+SimpleCov.start 'rails' do
+  command_name 'RSpec'
+  # coverage_dir 'coverage/rspec'
+  add_filter '/spec/'
+  add_filter '/config/'
+  add_filter '/vendor/'
+  add_filter '/db/'
+  add_group 'Models', 'app/models'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Services', 'app/services'
+  add_group 'Helpers', 'app/helpers'
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
